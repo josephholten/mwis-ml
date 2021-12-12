09d79991b18cb66ad87d26dc823497b771bde2e8
Joseph Holten <joseph@holten.com>
2021-12-12 11:23:53 +0100

    fixed ml-reduce with new ml_reducer interface

--- app/ml-reduce.cpp
+++ app/ml-reduce.cpp
@@ -22,16 +22,17 @@ int main(int argn, char** argv) {
     float q = 0.95;  // TODO: commandline parameter
 
     mis_config.graph_filename = graph_filepath.substr(graph_filepath.find_last_of('/') + 1);
+    mis_config.q = q;
 
     // Read the graph
     graph_access G;
     graph_io::readGraphWeighted(G, graph_filepath);
 
-    ml_reducer reducer = ml_reducer(mis_config, q);
+    ml_reducer reducer = ml_reducer(G, mis_config);
     graph_access R;
     std::vector<NodeID> reverse_mapping;
     //reducer.ml_reduce_old(G, R, reverse_mapping);
-    auto weight = reducer.iterative_reduce(G);
+    auto weight = reducer.iterative_reduce();
     std::cout << "Found independent set of weight: " << weight << std::endl;
 
     graph_io::writeIndependentSet(G, "/home/joseph/sem5/christian/mwis-ml/independent_set.mis");
